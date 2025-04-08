c4cc3a0990ef59aee2b93608912a6942a280cf05
atulsinghhhh <atulsinghhh25@gmail.com>
2025-04-08 18:49:41 +0530

    learning about controller

--- src/app.js
+++ src/app.js
@@ -15,4 +15,11 @@ app.use(express.urlencoded({extended: true, limit: "15kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+import userRouter from './routes/user.routes.js';
+
+
+// routes declaration
+app.use("/users",userRouter)
+
+
 export {app};
